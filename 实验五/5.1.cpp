40abbeb78f84069e532faa54525799663c85eeb7
Yrd980 <111880576+Yrd980@users.noreply.github.com>
2023-01-05 18:13:45 +0800

    Add files via upload

---
+++ 实验五/5.1.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+class Time
+{
+private:
+	int hour;
+	int minute;
+	int sec;
+public:
+	void shuru() {
+		cin >> hour >> minute >> sec;
+	}
+	void display() {
+		cout << hour << ":" << minute << ":" << sec << endl;
+	}
+};
+int main()
+{
+	Time t1;//¶¨Òåt1
+	t1.shuru();
+	t1.display();
+	return 0;
+}
